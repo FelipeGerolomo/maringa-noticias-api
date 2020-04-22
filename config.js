c2dc7f670864a057f57c6f3c3e75026bc0af1d35
Felipe Gerolomo <felipegerolomo@gmail.com>
2020-04-22 19:13:13 -0300

    mongo api

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ const config = {
     port: process.env.PORT || 8080
   },
   mongo: {
-    url: process.env.MONGO_DB_URI || 'mongodb://localhost/maringa-noticias'
+    url: process.env.MONGO_DB_URI || 'mongodb://gerolomo:gerolomo5298@maringa-noticias-shard-00-00-y6ngn.gcp.mongodb.net:27017,maringa-noticias-shard-00-01-y6ngn.gcp.mongodb.net:27017,maringa-noticias-shard-00-02-y6ngn.gcp.mongodb.net:27017/maringa-noticias?ssl=true&replicaSet=maringa-noticias-shard-0&authSource=admin&retryWrites=true&w=majority'
   }
 }
 
