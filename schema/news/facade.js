109c352b36405e92ebaabb8f43085244e5ef3b26
DESKTOP-RLN3DG3\Felipe <felipegerolomo@gmail.com>
2021-04-16 15:16:05 -0300

    delete

--- schema/news/facade.js
+++
@@ -1,6 +0,0 @@
-const Facade = require('../../lib/facade')
-const newsSchema = require('./schema')
-
-class NewsFacade extends Facade {}
-
-module.exports = new NewsFacade('News', newsSchema)
--- schema/provider/controller.js
+++
@@ -1,8 +0,0 @@
-const Controller = require('../../lib/controller')
-const providerFacade = require('./facade')
-
-class ProviderController extends Controller {
-    
-}
-
-module.exports = new ProviderController(providerFacade)
