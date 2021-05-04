56252bea52ee65801d3243a339b287d31158d4d6
DESKTOP-RLN3DG3\Felipe <felipegerolomo@gmail.com>
2021-05-03 22:21:27 -0300

    maringa-post test

--- src/main/java/br/com/maringanoticias/controller/CrawlerController.java
+++ src/main/java/br/com/maringanoticias/controller/CrawlerController.java
@@ -2,6 +2,8 @@ package br.com.maringanoticias.controller;
 
 
 import br.com.maringanoticias.crawler.CrawlerService;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -17,8 +19,14 @@ public class CrawlerController {
     CrawlerService crawlerService;
 
     @RequestMapping(method = RequestMethod.GET, value = "")
-    public boolean findMostWeekViewed() throws IOException {
+    public boolean start() throws IOException {
         this.crawlerService.start();
         return true;
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/maringa-post")
+    public String findMostWeekViewed() throws IOException {
+        Document doc = Jsoup.connect("https://maringapost.com.br/").get();
+        return doc.toString();
     }
 }
