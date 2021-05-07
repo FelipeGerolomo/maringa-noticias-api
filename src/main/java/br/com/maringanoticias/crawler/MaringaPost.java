a89111d35cfb3d051be963aa590ddb398738a60b
DESKTOP-RLN3DG3\Felipe <felipegerolomo@gmail.com>
2021-05-07 10:53:37 -0300

    ajuste maringa post

--- src/main/java/br/com/maringanoticias/crawler/MaringaPost.java
+++ src/main/java/br/com/maringanoticias/crawler/MaringaPost.java
@@ -4,6 +4,7 @@ import br.com.maringanoticias.domain.news.NewsDTO;
 import br.com.maringanoticias.domain.news.NewsService;
 import br.com.maringanoticias.domain.sourcenews.SourceNewsDTO;
 import br.com.maringanoticias.domain.sourcenews.SourceNewsService;
+import org.apache.commons.lang3.StringUtils;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -34,12 +35,14 @@ public class MaringaPost {
         String website = sourceNews.getDsUrl();
         Document doc = Jsoup.connect(website).userAgent("Mozilla").get();
         Elements elements = doc.select("div.td_module_flex_1.td_module_wrap.td-animation-stack");
+
         for (Element element : elements) {
             NewsDTO news = new NewsDTO();
             news.setDsTitle(element.select("div.td-module-meta-info > h3.entry-title > a").text());
             news.setDsUrl(element.select("div.td-module-meta-info > h3.entry-title > a").attr("href"));
             String ImageUrl = element.select("div.td-image-container a span ").attr("style");
-            news.setDsImageUrl(ImageUrl.substring( ImageUrl.indexOf("https://"), ImageUrl.indexOf(")") ));
+            ImageUrl = StringUtils.substringBetween(ImageUrl, "(", ")");
+            news.setDsImageUrl(ImageUrl);
             news.setDsDescription(element.select("div.td-module-meta-info > div.td-excerpt").text());
             news.setSourceNews(sourceNews);
             if (!this.newsService.isExistNews(news)) {
